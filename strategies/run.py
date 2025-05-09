e7d3f61779dfbf9665ad307f3f7bea67a7f171fd
marsvo1ta <euromobilco@gmail.com>
2025-05-09 17:59:55 +0300

    user

--- strategies/run.py
+++ strategies/run.py
@@ -4,7 +4,7 @@ from strategies.simple_strategy import simple_buy_strategy, simple_sell_strategy
 
 # USER_ID = "ddec577e-b919-442b-b76d-a2c97c7aabcd" Stage User
 SYMBOL = "BTCUSDT"
-USER_ID = "ab49d834-847c-4ffd-959e-922decd59e40" #Prod User
+USER_ID = "dea9ff14-e356-4539-b7fb-423dd44b4804" #Prod User
 
 async def run():
     async for db in get_session():
