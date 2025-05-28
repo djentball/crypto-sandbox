2f360459985346578cc1d16254c28e91539deee2
marsvo1ta <euromobilco@gmail.com>
2025-05-29 00:13:34 +0300

    clean wallets util

--- strategies/indicators_rsi.py
+++ strategies/indicators_rsi.py
@@ -1,8 +1,8 @@
 import requests
-from db.indicators_util import calculate_rsi, calculate_ema, fetch_binance_data
+from db.indicators_util import calculate_rsi, fetch_binance_data
 
 
-def generate_signal(close, ema_9, ema_21, rsi, user_id="c6a5c630-2ab6-423b-aa02-4ab69d1da6e3"):
+def generate_signal(rsi, user_id="c6a5c630-2ab6-423b-aa02-4ab69d1da6e3"):
     signal = "WAIT"
     if rsi:
         if rsi < 42:
@@ -19,13 +19,11 @@ def generate_signal(close, ema_9, ema_21, rsi, user_id="c6a5c630-2ab6-423b-aa02-
 
 
 timestamps, closes = fetch_binance_data()
-ema_9 = calculate_ema(closes, 9)
-ema_21 = calculate_ema(closes, 21)
 rsi = calculate_rsi(closes, 14)
 
 for i in range(len(closes) - 10, len(closes)):
     print(
-        f"{i}: close={closes[i]:.2f}, EMA_9={ema_9[i]}, EMA_21={ema_21[i]}, RSI={rsi[i]}"
+        f"{i}: close={closes[i]:.2f}, RSI={rsi[i]}"
     )
-    signal = generate_signal(closes[i], ema_9[i], ema_21[i], rsi[i])
+    signal = generate_signal(rsi[i])
     print("   Signal:", signal)
