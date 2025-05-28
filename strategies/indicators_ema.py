94dc35cb046e9806ec3c833008f6b63fffba2978
marsvo1ta <euromobilco@gmail.com>
2025-05-28 17:21:38 +0300

    clean wallets util

--- strategies/indicators_ema.py
+++ strategies/indicators_ema.py
@@ -7,14 +7,19 @@ timestamps, closes = fetch_binance_data()
 ema_9 = calculate_ema(closes, 9)
 ema_21 = calculate_ema(closes, 21)
 user_id = "07e6fb06-2491-4ebd-8435-547f052b0047"
+quantity = 0.0007
 
 if ema_9 > ema_21:
     requests.post('https://api-aio.alwaysdata.net/crypto/trade/buy', json={
-        "user_id": user_id, "symbol": "BTCUSDT", "quantity": 0.0007
+        "user_id": user_id, "symbol": "BTCUSDT", "quantity": quantity
     })
     signal = "BUY"
 elif ema_9 < ema_21:
     requests.post('https://api-aio.alwaysdata.net/crypto/trade/sell', json={
-        "user_id": user_id, "symbol": "BTCUSDT", "quantity": 0.0007
+        "user_id": user_id, "symbol": "BTCUSDT", "quantity": quantity
     })
     signal = "SELL"
+else:
+    signal = "WAIT"
+
+print(signal)
